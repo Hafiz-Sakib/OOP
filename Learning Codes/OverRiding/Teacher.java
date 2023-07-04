4602f2c17bb46a4dc0874e56c6d4193fa933a17e
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-07-04 21:48:33 +0600

    Constructor OverRiding

---
+++ Learning Codes/OverRiding/Teacher.java
@@ -0,0 +1,27 @@
+
+public class Teacher {
+
+    String name, gender;
+    int phn;
+
+    Teacher() {
+        System.out.println("No Information!");
+    }
+
+    Teacher(String n, String g) {
+        name = n;
+        gender = g;
+    }
+
+    Teacher(String n, String g, int p) {
+        name = n;
+        gender = g;
+        phn = p;
+    }
+
+    void displayTeacher() {
+        System.out.println("Name :" + name);
+        System.out.println("Gender :" + gender);
+        System.out.println("Phone No : " + phn);
+    }
+}
