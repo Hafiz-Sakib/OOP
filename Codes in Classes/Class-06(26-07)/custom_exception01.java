4549fe76bdb6985bde106fe7658ee0b9c618908d
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-08-02 20:58:56 +0600

    Performance Test Code Updates

--- Codes in Classes/Class-06(26-07)/custom_exception01.java
+++ Codes in Classes/Class-06(26-07)/custom_exception01.java
@@ -1,10 +1,10 @@
-import java.util.InputMismatchException;
+// import java.util.InputMismatchException;
 
 public class custom_exception01 {
-    public double div(int a, int b) throws ArithmeticException, InputMismatchException {
+    public double div(int a, int b) throws ArithmeticException {
         double d;
-        if (a < 0) {
-            throw new java.util.InputMismatchException("Value Can't be less than 0");
+        if (b < 0) {
+            throw new ArithmeticException();
         } else {
             d = (double) a / b;
             return d;
@@ -12,19 +12,19 @@ public class custom_exception01 {
     }
 
     public static void main(String[] args) {
-        custom_exception01 divison = new custom_exception01();
+        custom_exception01 division = new custom_exception01();
         /*
          * try
          * {
-         * double x = divison.div(-1, 2);
+         * double x = division.div(-1, 2);
          * System.out.println(x);
          * }
-         * catch (ArithmeticException | InputMismatchException err)
+         * catch (ArithmeticException)
          * {
          * System.out.println(err);
          * }
          */
-        double x = divison.div(1, 0);
+        double x = division.div(10, 0);
         System.out.println(x);
     }
 }
--- Codes in Classes/Class-06(26-07)/performance1.java
+++ Codes in Classes/Class-06(26-07)/performance1.java
@@ -17,5 +17,4 @@ public class performance1 {
         sc.close();
 
     }
-
 }
