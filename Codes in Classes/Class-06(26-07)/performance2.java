d43b40d5764c0fafabbff013e162772ad5c45beb
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-08-02 20:39:42 +0600

    Performance Test Code Updates

--- Codes in Classes/Class-06(26-07)/performance2.java
+++ Codes in Classes/Class-06(26-07)/performance2.java
@@ -9,5 +9,15 @@ public class performance2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Final Number : ");
+        performance2 check = new performance2();
+        int n = sc.nextInt();
+        try {
+            check.checkCourse(n);
+            System.out.println("You Can take retake this course !!");
+        } catch (myException err) {
+            System.err.println("Sorry ! You must take recourse !!");
+        }
+        sc.close();
     }
 }
