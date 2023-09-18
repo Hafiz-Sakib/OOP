9909b1b16739f85f9982216d5218ea96dbd7e896
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-09-18 09:42:22 +0600

    Lab FInal Resources Added

--- Lab Reports/All/Main.java
+++ Lab Reports/All/Main.java
@@ -1,57 +1,46 @@
-class Vehicle {
-    String make;
-    String model;
-    int year;
-    String fuelType;
 
-    public Vehicle(String make, String model, int year, String fuelType) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        this.fuelType = fuelType;
+interface Shape {
+    double calculateArea();
+}
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
     }
 }
 
-class Truck extends Vehicle {
-    int capacityInTons;
+class Rectangle implements Shape {
+    private double width;
+    private double height;
 
-    public Truck(String make, String model, int year, String fuelType, int capacityInTons) {
-        super(make, model, year, fuelType);
-        this.capacityInTons = capacityInTons;
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
     }
-}
 
-class Car extends Vehicle {
-    int numberOfDoors;
-
-    public Car(String make, String model, int year, String fuelType, int numberOfDoors) {
-        super(make, model, year, fuelType);
-        this.numberOfDoors = numberOfDoors;
-    }
-}
-
-class Motorcycle extends Vehicle {
-    boolean hasFairing;
-
-    public Motorcycle(String make, String model, int year, String fuelType, boolean hasFairing) {
-        super(make, model, year, fuelType);
-        this.hasFairing = hasFairing;
+    public double calculateArea() {
+        return width * height;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Truck truck = new Truck("Ford", "F-150", 2022, "Gasoline", 2);
-        Car car = new Car("Toyota", "Camry", 2023, "Hybrid", 4);
-        Motorcycle motorcycle = new Motorcycle("Harley-Davidson", "Sportster", 2021, "Gasoline", true);
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        System.out.println("Area of Circle: " + circle.calculateArea());
+        System.out.println("Area of Rectangle: " + rectangle.calculateArea());
 
-        System.out.println("Truck Details:\nMaker: " + truck.make + "\nTruck Model: " + truck.model + "\nRelease Year: "
-                + truck.year + "\nFuel Type: " + truck.fuelType + "\nCapacity In Ton's: " + truck.capacityInTons
-                + "\n");
-        System.out.println("Car Details:\nMaker: " + car.make + "\nCar Model: " + car.model + "\nRelease Year: "
-                + car.year + "\nFuel Type: " + car.fuelType + "\nNumber of Doors: " + car.numberOfDoors + "\n");
-        System.out.println("Motorcycle Details:\nMaker: " + motorcycle.make + "\nMotorcycle Model: " + motorcycle.model
-                + "\nRelease Year: " + motorcycle.year + "\nFuel Type: " + motorcycle.fuelType + "\nHas Fairing: "
-                + (motorcycle.hasFairing ? "Yes" : "No") + "\n");
+        Shape shape1 = circle;
+        Shape shape2 = rectangle;
+
+        System.out.println("Area of Shape (Circle): " + shape1.calculateArea());
+        System.out.println("Area of Shape (Rectangle): " + shape2.calculateArea());
     }
 }
